8e3066bdde6709f2160bec530a2c96d9a3a08ee3
Joshua Mba <angelicjoekeyz@gmail.com>
2020-07-30 21:29:11 +0100

    Second commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
- * This is a Moive Rental Api demo created by Joshua Mba
+ * This is a Moive Rental App created by Joshua Mba
  * 
  */
 
@@ -7,28 +7,11 @@ const search_btn = document.getElementById('search-btn');
 const rent_btn = document.getElementById('rent-btn');
 const movie_table = document.getElementById('movie-table');
 
-//movie array
-const movies = [{
-        title: "Masked Idiot",
-        genre: "Comedy",
-        year: 2020,
-        rented: false
-    },
-    {
-        title: "Bad boys for Life",
-        genre: "Action",
-        year: 2020,
-        rented: false,
-    },
-    {
-        title: "Titans",
-        genre: "Action",
-        year: 2024,
-        rented: false,
-    }
-]
 
-console.log(movies);
+
+// console.log(movies);
+
+
 class Movieslibrary {
     constructor(movies) {
         this.movies = movies
@@ -41,10 +24,12 @@ class Movieslibrary {
                     movie_table.insertAdjacentHTML(
                         'beforeend',
 
-                        `<td>${movie.title}</td>
+                        `<tr id= "${index}">
+                            <td>${movie.title}</td>
                             <td>${movie.genre}</td>
                             <td>${movie.year}</td>
-                            <td><button onclick="movie.rent_movie(${index})">Rent Now</button></td>`
+                            <td><button onclick="movie.rent_movie(${index})">Rent Now</button></td>
+                        </tr>`
                     );
                 }
             });
@@ -53,13 +38,27 @@ class Movieslibrary {
     rent_movie(index) {
         this.movies[index].rented = true;
         alert('Successfully Rented');
-        // movie.list_movies();
+        let movie_row = document.getElementById(index);
+        // console.log(movie_row);
+        // console.log(index);
+        movie_row.classList.add('disable');
 
+    }
+
+    search_movie() {
+
+
+    }
+
+    restore_movie() {
+        let all_rows = document.querySelectorAll('tr');
+        all_rows.forEach(function(element) {
+            element.classList.remove('disable')
+        });
     }
 }
 
 let movie = new Movieslibrary(movies);
 
-//function to call rent method
 
 // let rent = movie.rent_movie()
---
+++ movies.js
@@ -0,0 +1,19 @@
+const movies = [{
+        title: "Masked Idiot",
+        genre: "Comedy",
+        year: 2020,
+        rented: false
+    },
+    {
+        title: "Bad boys for Life",
+        genre: "Action",
+        year: 2020,
+        rented: false,
+    },
+    {
+        title: "Titans",
+        genre: "Action",
+        year: 2024,
+        rented: false,
+    }
+]
