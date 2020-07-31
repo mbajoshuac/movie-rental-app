b1dd6dc1ff2161a18514b90f729e7f7607127d8e
Joshua Mba <angelicjoekeyz@gmail.com>
2020-07-31 16:09:58 +0100

    FInal commit

--- index.js
+++ index.js
@@ -1,64 +1,67 @@
-/**
+/*
  * This is a Moive Rental App created by Joshua Mba
- * 
  */
 
-const search_btn = document.getElementById('search-btn');
 const rent_btn = document.getElementById('rent-btn');
-const movie_table = document.getElementById('movie-table');
-
-
+const movie_table = document.getElementById('movie-body');
+const search_input = document.getElementById('search');
 
 // console.log(movies);
 
-
+//Movie Class and methods
 class Movieslibrary {
     constructor(movies) {
-        this.movies = movies
-    }
-    list_movies() {
-        if (movies.length > 0) {
-            movies.map((movie, i) => {
-                let index = this.movies.indexOf(movie);
-                if (movie.rented === false) {
-                    movie_table.insertAdjacentHTML(
-                        'beforeend',
+        this.movies = movies;
+        this.list_movies(this.movies);
 
-                        `<tr id= "${index}">
+    }
+
+    list_movies(movies_array) {
+        movies_array.map((movie, index) => {
+            movie_table.insertAdjacentHTML(
+                'beforeend',
+
+                `<tr id= "${index}">
                             <td>${movie.title}</td>
                             <td>${movie.genre}</td>
                             <td>${movie.year}</td>
-                            <td><button onclick="movie.rent_movie(${index})">Rent Now</button></td>
+                            <td><button onclick="alert('You have successfully Rented ${movie.title} Movie '); movie.rent_movie(${index})">Rent Now</button></td>
                         </tr>`
-                    );
-                }
-            });
-        }
+            );
+            if (movie.rented) {
+                this.rent_movie(index)
+            }
+        });
     };
     rent_movie(index) {
         this.movies[index].rented = true;
-        alert('Successfully Rented');
         let movie_row = document.getElementById(index);
-        // console.log(movie_row);
-        // console.log(index);
         movie_row.classList.add('disable');
 
     }
 
     search_movie() {
-
-
+        search_input.oninput = (e) => {
+            let string = e.target.value;
+            string = string.toLowerCase();
+            let filterd_movies = this.movies.filter(movie => movie.title.toLowerCase().includes(string));
+            movie_table.innerHTML = "";
+            this.list_movies(filterd_movies);
+            // console.log(filterd_movies);
+        }
     }
-
     restore_movie() {
-        let all_rows = document.querySelectorAll('tr');
-        all_rows.forEach(function(element) {
-            element.classList.remove('disable')
-        });
+        this.movies.map(movie => {
+            if (movie.rented === true) {
+                movie.rented = false;
+            }
+        })
+        movie_table.innerHTML = "";
+        this.list_movies(this.movies)
     }
 }
 
+//invoking the Moive Class
 let movie = new Movieslibrary(movies);
-
-
-// let rent = movie.rent_movie()
+//calling the search method 
+movie.search_movie();
--- movies.js
+++ movies.js
@@ -1,19 +1,34 @@
+/*
+The movie array
+*/
 const movies = [{
-        title: "Masked Idiot",
-        genre: "Comedy",
+        title: "Mulan",
+        genre: "Action",
         year: 2020,
         rented: false
     },
     {
         title: "Bad boys for Life",
-        genre: "Action",
+        genre: "Action/comedy",
         year: 2020,
         rented: false,
     },
     {
         title: "Titans",
         genre: "Action",
-        year: 2024,
+        year: 2018,
+        rented: false,
+    },
+    {
+        title: "Tenet",
+        genre: "Action",
+        year: 2020,
+        rented: false,
+    },
+    {
+        title: "Wonder",
+        genre: "Action",
+        year: 2019,
         rented: false,
     }
 ]
